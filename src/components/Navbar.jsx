82d957cd6a556fbaed5b9b24dcdcc99be5e2b2ba
trimzymberi2 <trimzymberi22@gmail.com>
2024-05-27 03:12:45 +0200

    min changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,13 +16,13 @@ export default function Navbar() {
             <img className='flex items-center  justify-center w-14 h-12' src={logo} alt="Matchy" />
         </div>
         <div className=' md:opacity-100 hidden md:flex md:flex-row lg:gap-16 gap-8 text-primary'>
-                <h4 className='text-xl hover:text-primaryshadow hover:cursor-pointer'>HOME</h4>
-                <h4 className='text-xl hover:text-primaryshadow hover:cursor-pointer'>GARDEROBE</h4>
-                <h4 className='text-xl hover:text-primaryshadow hover:cursor-pointer'>OUTFIT</h4>
+                <h4 className='text-xl hover:text-primaryshadow hover:cursor-pointer transform hover:scale-110 transition-all duration-300 ease-in-out'>HOME</h4>
+                <h4 className='text-xl hover:text-primaryshadow hover:cursor-pointer transform hover:scale-110 transition-all duration-300 ease-in-out'>GARDEROBE</h4>
+                <h4 className='text-xl hover:text-primaryshadow hover:cursor-pointer transform hover:scale-110 transition-all duration-300 ease-in-out'>OUTFIT</h4>
         </div>
         <img src={icon} onClick={toggleNavbar} alt="" className='hover:cursor-pointer h-10 md:hidden absolute top-5 right-0'/>
         <div className='flex items-center gap-5'>
-            <button className='w-24 hover:bg-primaryshadow h-10 md:flex md:items-center md:justify-center hidden bg-primary rounded-lg text-white'>
+            <button className='w-24 transform hover:scale-110 transition-all duration-300 ease-in-out hover:bg-primaryshadow h-10 md:flex md:items-center md:justify-center hidden bg-primary rounded-lg text-white'>
               Log in
             </button>
         </div>
